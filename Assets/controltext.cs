b2c0a38dcdc68d630a70ec1159851dafcf93cd28
yakutdmitry <D23125246@mytudublin.ie>
2024-05-10 02:56:25 +0100

    Tutorial level almost ready

--- Assets/controltext.cs
+++ Assets/controltext.cs
@@ -1,10 +1,9 @@
+using System.Security.Cryptography;
 using UnityEngine;
 using TMPro;
 
 public class DisableTextAfterDelay : MonoBehaviour
 {
-    public TextMeshProUGUI textToDisable;
-
     private bool playerInputDetected = false;
 
     void Update()
@@ -15,21 +14,9 @@ public class DisableTextAfterDelay : MonoBehaviour
             playerInputDetected = true;
 
             // Call the DisableText method after 5 seconds from the first player input
-            Invoke("DisableText", 5f);
+            Destroy(gameObject, 5f);
         }
     }
 
-    void DisableText()
-    {
-        // Check if the TextMeshPro game object is valid
-        if (textToDisable != null)
-        {
-            // Disable the TextMeshPro game object
-            textToDisable.gameObject.SetActive(false);
-        }
-        else
-        {
-            Debug.LogWarning("TextMeshPro game object not assigned!");
-        }
-    }
+    
 }
